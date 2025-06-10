3cefe15f59ea2d793e577c64ac9b9ed061b7a4b8
Elias Vera <evera@roshka.com>
2025-06-10 17:13:24 -0300

    adding a file verificator

--- src/main/java/org/platzi/Main.java
+++ src/main/java/org/platzi/Main.java
@@ -1,7 +1,30 @@
 package org.platzi;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        /*TODO ESTRUCTURA
+        * id
+        * description
+        * status
+        */
+        Main app = new Main();
+        app.fileVerificator();
+
+        if(args.length > 0){
+            System.out.println("Hay commando");
+        } else {
+            System.out.println("No hay commando");
+        }
+
+    }
+
+    private void fileVerificator() throws IOException {
+        File file = new File("tasks.json");
+        if (!file.exists()){
+            file.createNewFile();
+        }
     }
 }
